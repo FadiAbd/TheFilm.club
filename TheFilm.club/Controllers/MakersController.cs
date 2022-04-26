ea61f8d104b5f9b0d431f45abbfd267f87dd8509
fadi abdallah <fadishadow@hotmail.com>
2022-04-26 08:28:05 +0200

    Added IndexAndDetails

---
+++ TheFilm.club/Controllers/MakersController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using TheFilm.club.Data;
+using TheFilm.club.Models;
+using TheFilm.club.ViewModels;
+
+namespace TheFilm.club.Controllers
+{
+    public class MakersController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public MakersController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            var viewModel = new MakersIndexViewModel();
+            viewModel.Makers = _dbContext.Makers.Include(n => n.Films)
+                .Select(dbPro => new MakerViewModel
+                {
+                    Id = dbPro.Id,
+                    Name = dbPro.Name,
+                    Picture = dbPro.Picture,
+                    Biography = dbPro.Biography
+                }).ToList();
+            return View(viewModel);
+        }
+        //Get/Create
+        public IActionResult Create()
+        {
+            var viewModel = new MakersNewViewModel();
+
+            return View(viewModel);
+        }
+        [HttpPost]
+        public IActionResult Create(MakersNewViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var dbPro = new Maker();
+                _dbContext.Makers.Add(dbPro);
+                dbPro.Name = viewModel.Name;
+                dbPro.Picture = viewModel.Picture;
+                dbPro.Biography = viewModel.Biography;
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(viewModel);
+        }
+    }
+}
---
+++ TheFilm.club/ViewModels/MakersNewViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheFilm.club.ViewModels
+{
+    public class MakersNewViewModel
+    {
+        public string Picture { get; set; }
+        [Display(Name = "Name")]
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Biography")]
+        [Required]
+        public string Biography { get; set; }
+    }
+}
