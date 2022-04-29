aa22a1593e3c29ac8936f01b0529d45a7c14241b
fadi abdallah <fadishadow@hotmail.com>
2022-04-29 20:05:29 +0200

    adding the Basket class

--- TheFilm.club/Data/Basket/Basket.cs
+++ TheFilm.club/Data/Basket/Basket.cs
@@ -15,14 +15,56 @@ namespace TheFilm.club.Data.Basket
             _dbContext = dbContext;
         }
 
+      
         public List<BasketItem> GetBasketItems()
         {
             return BasketItems ?? (BasketItems = _dbContext.BasketItems.Where(n => n.BasketId ==
             BasketId).Include(n => n.Film).ToList());
         }
 
+        public void AddItemToBasket(Film film)
+        {
+            var basketItem = _dbContext.BasketItems.FirstOrDefault(n => n.Film.Id == film.Id && n.BasketId == BasketId);
+
+            if (basketItem == null)
+            {
+                basketItem = new BasketItem()
+                {
+                    BasketId = BasketId,
+                    Film = film,
+                    Amount = 1
+                };
+                _dbContext.BasketItems.Add(basketItem);
+            }
+            else
+            {
+                basketItem.Amount++; 
+            }
+            _dbContext.SaveChanges();
+        }
+
+        public void RemoveItemFromBasket(Film film)
+        {
+            var basketItem = _dbContext.BasketItems.FirstOrDefault(n => n.Film.Id == film.Id && n.BasketId == BasketId);
+
+            if (basketItem != null)
+            {
+                if (basketItem.Amount > 1)
+                {
+                    basketItem.Amount--;
+                }
+                else
+                {
+                    _dbContext.BasketItems.Remove(basketItem);
+                }
+               
+            }
+            _dbContext.SaveChanges();
+        }
         public double GetBasketTotal() => _dbContext.BasketItems.Where(n => n.BasketId ==
         BasketId).Select(n => n.Film.Price * n.Amount).Sum();
+
+
 
     }
 }
