0613d2648baad1aa34c8a22dc85ece26f3913c24
fadi abdallah <fadishadow@hotmail.com>
2022-04-29 10:45:21 +0200

    fixes in delete/Artists/Makers/Theaters Removed picture

--- TheFilm.club/ViewModels/ArtistsDeleteViewModel.cs
+++ TheFilm.club/ViewModels/ArtistsDeleteViewModel.cs
@@ -3,5 +3,7 @@
     public class ArtistsDeleteViewModel
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public string Biography { get; set; }
     }
 }
